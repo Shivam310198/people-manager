7e3b4991ef76672628410723213b7b4df49d6e19
Shivam Agrawal <shivam310198@gmail.com>
2026-08-20 03:03:41 +0530

    Complete_App

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173").AllowAnyMethod().AllowAnyHeader(); // x-pagination-header
+                      });
+});
+
+builder.Services.AddControllers();
+
+String connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new ArgumentNullException("Connection string is Null.");
+builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlite(connectionString));
+var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapControllers();
+
+app.Run();
