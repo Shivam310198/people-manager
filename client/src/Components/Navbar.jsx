2a954e9fa705ecb499c4a9cd109d6d37d604b2df
Shivam Agrawal <shivam310198@gmail.com>
2026-08-20 10:52:44 +0530

    Instruction on Home Page

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -31,9 +31,15 @@ const Navbar = () => {
                                     }`}
 
                             >
-                                Home
+                                Home <p>
+                                Open side menu, if using mobile phone, by clicking the hamburger icon on the top right corner of the page. Navigate to the "Person" page to view the list of people. You can add, edit, or delete people from this page. The "About" page provides information about the application and its features.
+                            </p>
+                            <p>
+                                OR</p>
+                            <p>
+                                Use the navigation links above to switch between the Home, About, and Person pages. The active link will be highlighted with a gradient background for better visibility.
+                            </p>
                             </NavLink>
-
                             <NavLink
                                 to='/about'
 
